d911544ae5835dd612c4ae1c05c5ea3dbd93371b
ascrutae <nzt48.xin@gmail.com>
2017-12-19 16:24:09 +0800

    add httpclient scenario

---
+++ httpclient-scenario/src/main/java/org/skywalking/apm/testcase/httpclient/ServletForContextPropagate.java
@@ -0,0 +1,23 @@
+package org.skywalking.apm.testcase.httpclient;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletForContextPropagate extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        PrintWriter out = resp.getWriter();
+        out.print("{'test':'test'}");
+        out.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
