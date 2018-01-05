3ceb77b8d4be68c91d0f3348e364a9f17f82bc4a
ascrutae <nzt48.xin@gmail.com>
2018-01-05 16:30:53 +0800

    remove resttemplate 3.0 version

--- spring-scenario/spring-projectC/src/main/java/org/apache/skywalking/apm/testcase/spring3/controller/CaseController.java
+++ spring-scenario/spring-projectC/src/main/java/org/apache/skywalking/apm/testcase/spring3/controller/CaseController.java
@@ -19,9 +19,7 @@
 package org.apache.skywalking.apm.testcase.spring3.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
