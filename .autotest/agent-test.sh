79ed7aa30ed169b7084b6518477e5ab3ce4f9cc8
Xin,Zhang <zhangxin@apache.org>
2018-03-22 21:08:57 +0800

    Feature/logs support (#16)
    * add logs

--- .autotest/agent-test.sh
+++ .autotest/agent-test.sh
@@ -114,6 +114,11 @@ PRG="$0"
 PRGDIR=`dirname "$PRG"`
 [ -z "$AGENT_TEST_HOME" ] && AGENT_TEST_HOME=`cd "$PRGDIR/.." >/dev/null; pwd`
 WORKSPACE_DIR="$AGENT_TEST_HOME/workspace"
+#
+# log dir
+#
+LOGS_DIR="$WORKSPACE_DIR/logs"
+#
 SOURCE_DIR="$WORKSPACE_DIR/sources"
 #
 TEST_CASES_DIR="$AGENT_TEST_HOME/testcases" # Testcase dir
@@ -191,6 +196,13 @@ echo "current test case branch: ${TEST_CASES_BRANCH}. current test case commit i
 #
 echo "clear Workspace"
 clearWorkspace
+#
+# create log directory
+#
+LOGS_DIR="$WORKSPACE_DIR/logs"
+if [ ! -d "$LOGS_DIR" ];then
+    mkdir -p "$LOGS_DIR"
+fi
 ############## build agent ##############
 #	1. checkout agent source code
 #	2. switch branch
@@ -375,7 +387,7 @@ java -DtestDate="$TEST_TIME" \
 	-DtestCasePath="$TEST_CASES_DIR" -DreportFilePath="$REPORT_DIR" \
 	-DcasesBranch="$TEST_CASES_BRANCH" -DcasesCommitId="${TEST_CASES_COMMITID}" \
 	-Dcommitter="$COMMITTER"	\
-	-jar $WORKSPACE_DIR/skywalking-autotest.jar
+	-jar $WORKSPACE_DIR/skywalking-autotest.jar > $LOGS_DIR/validate-$TEST_TIME.log
 
 if [ ! -f "$REPORT_DIR/${AGENT_GIT_BRANCH}" ]; then
 	mkdir -p $REPORT_DIR/${AGENT_GIT_BRANCH}
