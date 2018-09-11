57b260f9c08cdbc47c58443acd69140f45b9abb3
ascrutae <nzt48.xin@gmail.com>
2018-09-11 16:16:19 +0800

    Modify grpc port

--- sofarpc-scenario/sofarpc-provider/bin/sofarpc-service.sh
+++ sofarpc-scenario/sofarpc-provider/bin/sofarpc-service.sh
@@ -19,6 +19,6 @@ do
     CLASSPATH="$i:$CLASSPATH"
 done
 
-JAVA_OPTS="$JAVA_OPTS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -Dskywalking.collector.grpc_channel_check_interval=2 -Dskywalking.collector.app_and_service_register_check_interval=2 -Dskywalking.collector.discovery_check_interval=2 -Dskywalking.collector.servers=${COLLECTOR_SERVER} -Dcom.alipay.sofa.rpc.registry.address=zookeeper://${ZK_ADDRESS} -Dskywalking.agent.application_code=sofarpc-provider -Xms256m -Xmx256m -XX:PermSize=64M -XX:MaxPermSize=64"
+JAVA_OPTS="$JAVA_OPTS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -Dskywalking.collector.grpc_channel_check_interval=2 -Dskywalking.collector.app_and_service_register_check_interval=2 -Dskywalking.collector.discovery_check_interval=2 -Dskywalking.collector.backend_service=${COLLECTOR_SERVER} -Dcom.alipay.sofa.rpc.registry.address=zookeeper://${ZK_ADDRESS} -Dskywalking.agent.application_code=sofarpc-provider -Xms256m -Xmx256m -XX:PermSize=64M -XX:MaxPermSize=64"
 
 $JAVA_HOME/bin/java $JAVA_OPTS -classpath "$CLASSPATH" $SOFARPC_PROVIDER_OPTS org.apache.skywaking.apm.testcase.sofarpc.provider.ServerApplication
