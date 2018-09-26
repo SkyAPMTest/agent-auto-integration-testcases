caa50164fb514dd63ec7392e3302776ef635658a
Xin,Zhang <zhangxin@apache.org>
2018-09-27 04:52:47 +0800

    Merge feature/6.x to master (#33)
    * Fix the script paramster names
    
    * Add undertow-scenario (#29)
    
    * Add undertow-scenario
    
    * Add support_version 2.0.0.Final->2.0.13.Final for Undertow
    
    * Adjust expectedData for Undertow
    
    * Revert modify for 'agent-test.sh'
    
    * Change collector port
    
    * change collector paramater variables
    
    * Fix the link is 404

--- .autotest/agent-test.sh
+++ .autotest/agent-test.sh
@@ -435,7 +435,7 @@ fi
 if [ "$ISSUE_NO" = "UNKNOWN" ]; then
     echo "issue no is empty, ignore to push comment"
 else
-    curl --user ${GITHUB_ACCOUNT} -X POST -H "Content-Type: text/plain" -d "{\"body\":\"Here is the [test report](http://github.com/SkywalkingTest/agent-integration-test-report/tree/master/${TEST_TIME_YEAR}/${TEST_TIME_MONTH}/${COMMITTER}/testReport-${TEST_CASES_BRANCH}-${TEST_TIME}.md) and [validate logs](${VALIDATE_LOG_URL_PREFIX}/validate-$TEST_TIME.log)\"}" https://api.github.com/repos/apache/incubator-skywalking/issues/${ISSUE_NO}/comments
+    curl --user ${GITHUB_ACCOUNT} -X POST -H "Content-Type: text/plain" -d "{\"body\":\"Here is the [test report](http://github.com/SkywalkingTest/agent-integration-test-report/tree/master/${TEST_TIME_YEAR}/${TEST_TIME_MONTH}/${COMMITTER}/testReport-${NORMALIZED_TEST_CASES_BRANCH}-${TEST_TIME}.md) and [validate logs](${VALIDATE_LOG_URL_PREFIX}/validate-$TEST_TIME.log)\"}" https://api.github.com/repos/apache/incubator-skywalking/issues/${ISSUE_NO}/comments
 fi
 
 
