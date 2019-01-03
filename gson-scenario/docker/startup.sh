fe2b4763adf8b4c9b0f12b025392b781681811e4
XiaoFu <248628578@qq.com>
2019-01-03 23:21:13 +0800

    Add Gson-scenario (#44)
    * Support gson test case

---
+++ gson-scenario/docker/startup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export AGENT_FILE_PATH=/usr/local/skywalking-gson-scenario/agent
+if [ -f "${AGENT_FILE_PATH}/skywalking-agent.jar" ]; then
+    GSON_OPTS=" -javaagent:${AGENT_FILE_PATH}/skywalking-agent.jar -Dskywalking.collector.grpc_channel_check_interval=2 -Dskywalking.collector.app_and_service_register_check_interval=2 -Dcollector.discovery_check_interval=2 -Dskywalking.collector.backend_service=${COLLECTOR_SERVER} -Dskywalking.agent.service_name=gson-scenario -Dskywalking.plugin.gson.trace_dsl=true -Xms256m -Xmx256m -XX:PermSize=64M -XX:MaxPermSize=64"
+fi
+
+java $GSON_OPTS  -jar /usr/local/skywalking-gson-scenario/gson-scenario.jar
